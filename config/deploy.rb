54bb695433dafdbb2b9219f4d3405905498b4ab1
Minseok Bang <minseokbang@Minseokui-MacBook-Air.local>
2018-11-03 17:44:46 +0900

    1

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,10 +6,10 @@ set :repo_url, "git@github.com:seokk1209/test.git"
 set :deploy_to, "/root/test"
 
 set :rbenv_type, :user # or :system, depends on your rbenv setup 
-# set :rbenv_ruby, '2.4.0' 
-# set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec" 
-# set :rbenv_map_bins, %w{rake gem bundle ruby rails} 
-# set :rbenv_roles, :all # default values # Default value for :linked_files is [] 
+set :rbenv_ruby, '2.4.0' 
+set :rbenv_prefix, "RBENV_ROOT=/root/.rbenv RBENV_VERSION=2.4.0 /root/.rbenv/bin/rbenv exec" 
+set :rbenv_map_bins, %w{rake gem bundle ruby rails} 
+set :rbenv_roles, :all # default values # Default value for :linked_files is [] 
 
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml') 
 set :config_files, fetch(:linked_files) 
